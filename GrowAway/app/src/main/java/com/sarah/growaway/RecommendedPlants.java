81fa786ff06f70bfaba3e015b8128de6463e45be
Sarah <smfrost@ucsc.edu>
2019-11-08 10:25:06 -0800

    More updates

--- GrowAway/app/src/main/java/com/sarah/growaway/RecommendedPlants.java
+++ GrowAway/app/src/main/java/com/sarah/growaway/RecommendedPlants.java
@@ -67,7 +67,7 @@ public class RecommendedPlants extends AppCompatActivity {
 
 
         try {
-            Thread.sleep(10000);
+            Thread.sleep(20000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
