1d650221d631267a5c9fa9604f2947df2a135b9b
Brian Thyfault <brianthyfault@gmail.com>
2019-11-30 17:15:47 -0800

    Inital Commit

---
+++ GrowAway/app/src/main/java/com/Planted/growaway/FavoritedPlants.java
@@ -0,0 +1,14 @@
+package com.Planted.growaway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavoritedPlants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorited_plants);
+    }
+}
